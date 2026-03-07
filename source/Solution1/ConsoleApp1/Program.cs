b623aed62447172ec954effcbf69d83f0c33b41f
LimonLimL <limonlv99@gmail.com>
2026-03-07 10:27:34 +0700

    Add Program.cs with Position usage
    Created main program entry point

--- source/Solution1/ConsoleApp1/Program.cs
+++ source/Solution1/ConsoleApp1/Program.cs
@@ -1,4 +1,24 @@
-﻿
+﻿using System;
+using Domain.Position;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var position = new Position(
+                id: 1,
+                name: "Developer",
+                descreption: "Backend developer",
+                lifeTime: DateTime.UtcNow
+            );
+            
+            Console.WriteLine($"Position: {position.Name}");
+            Console.WriteLine($"Description: {position.Descreption}");
+        }
+    }
+}
 
 
 
