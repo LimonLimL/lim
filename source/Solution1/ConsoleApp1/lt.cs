772793b73018302a57ceed46ffb4d625daea43cc
LimonLimL <limonlv99@gmail.com>
2026-03-07 10:14:02 +0700

    Реализовал новую фичу

---
+++ source/Solution1/ConsoleApp1/lt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Position
+{
+    public class Position
+    {
+        public Position(Id id, Name name, Description descreption, EntityLifeTime lifeTime)
+        {
+            Id = id;
+            Name = name;
+            Descreption = descreption;
+            LifeTime = lifeTime;
+        }
+        public Id Id { get; }
+        public Name Name { get; }
+        public Description Descreption { get; }
+        public EntityLifeTime LifeTime { get; }
+    }
+}       
